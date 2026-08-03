c8410d74f46298c2c4a8d5cfd996491d218ee6f3
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-03 17:57:33 +0700

    feat: add Hello World (Java)

---
+++ programs/HelloWorld.java
@@ -0,0 +1,9 @@
+// HelloWorld.java
+// Program pertama dalam Java.
+// Kompilasi: javac HelloWorld.java && java HelloWorld
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
