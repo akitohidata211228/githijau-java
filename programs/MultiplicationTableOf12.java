713de6e4ab51a143068a344e0258cc23881a05f3
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 22:20:39 +0700

    feat: add Tabel Perkalian 12 (Java)

---
+++ programs/MultiplicationTableOf12.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf12.java
+// Tabel perkalian 12.
+
+public class MultiplicationTableOf12 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("12 x " + i + " = " + (12 * i));
+        }
+    }
+}
