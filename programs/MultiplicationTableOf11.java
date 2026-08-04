d4d65189250b57498ed5a7a3fd975941da6839c8
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 21:52:39 +0700

    feat: add Tabel Perkalian 11 (Java)

---
+++ programs/MultiplicationTableOf11.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf11.java
+// Tabel perkalian 11.
+
+public class MultiplicationTableOf11 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("11 x " + i + " = " + (11 * i));
+        }
+    }
+}
