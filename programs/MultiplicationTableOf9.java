2a76d1a1108a51af00d59f6343f48662d91f32cf
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 18:54:11 +0700

    feat: add Tabel Perkalian 9 (Java)

---
+++ programs/MultiplicationTableOf9.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf9.java
+// Tabel perkalian 9.
+
+public class MultiplicationTableOf9 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("9 x " + i + " = " + (9 * i));
+        }
+    }
+}
