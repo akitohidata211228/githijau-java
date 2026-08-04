3a62bea9ca9038d23483ba4759efe2bfa9384455
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 12:18:03 +0700

    feat: add Tabel Perkalian 2 (Java)

---
+++ programs/MultiplicationTableOf2.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf2.java
+// Tabel perkalian 2.
+
+public class MultiplicationTableOf2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+        }
+    }
+}
