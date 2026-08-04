5a9a9ddf0018b4a88318716d46f58a7f4ff1eca0
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 13:26:03 +0700

    feat: add Tabel Perkalian 3 (Java)

---
+++ programs/MultiplicationTableOf3.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf3.java
+// Tabel perkalian 3.
+
+public class MultiplicationTableOf3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("3 x " + i + " = " + (3 * i));
+        }
+    }
+}
