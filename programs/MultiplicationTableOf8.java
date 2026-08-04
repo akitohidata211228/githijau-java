5c41ea909a83842285addaeb7f722b3fb65b2eb3
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 18:54:03 +0700

    feat: add Tabel Perkalian 8 (Java)

---
+++ programs/MultiplicationTableOf8.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf8.java
+// Tabel perkalian 8.
+
+public class MultiplicationTableOf8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("8 x " + i + " = " + (8 * i));
+        }
+    }
+}
