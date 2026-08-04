78ba0f3c3ae14a571cdc3ef8a21fc7b6ba3a890e
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 15:13:03 +0700

    feat: add Tabel Perkalian 5 (Java)

---
+++ programs/MultiplicationTableOf5.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf5.java
+// Tabel perkalian 5.
+
+public class MultiplicationTableOf5 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
