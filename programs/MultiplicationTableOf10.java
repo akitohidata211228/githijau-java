0efe7b1baf2d6228cff4a699d9c119d6039db540
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 20:19:39 +0700

    feat: add Tabel Perkalian 10 (Java)

---
+++ programs/MultiplicationTableOf10.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf10.java
+// Tabel perkalian 10.
+
+public class MultiplicationTableOf10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("10 x " + i + " = " + (10 * i));
+        }
+    }
+}
