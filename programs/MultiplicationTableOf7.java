6a7e2eef900680218967f89edb44891fec070cb1
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 17:55:03 +0700

    feat: add Tabel Perkalian 7 (Java)

---
+++ programs/MultiplicationTableOf7.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf7.java
+// Tabel perkalian 7.
+
+public class MultiplicationTableOf7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("7 x " + i + " = " + (7 * i));
+        }
+    }
+}
