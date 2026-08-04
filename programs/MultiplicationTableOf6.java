a1eecd7f59c229cc4110744bfe72161a5aa45cfe
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-04 16:12:03 +0700

    feat: add Tabel Perkalian 6 (Java)

---
+++ programs/MultiplicationTableOf6.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf6.java
+// Tabel perkalian 6.
+
+public class MultiplicationTableOf6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("6 x " + i + " = " + (6 * i));
+        }
+    }
+}
