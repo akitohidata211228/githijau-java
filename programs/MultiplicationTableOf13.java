dfcf6905ee1f2c6068329654b9636204f0a272b1
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-05 08:00:39 +0700

    feat: add Tabel Perkalian 13 (Java)

---
+++ programs/MultiplicationTableOf13.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf13.java
+// Tabel perkalian 13.
+
+public class MultiplicationTableOf13 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("13 x " + i + " = " + (13 * i));
+        }
+    }
+}
