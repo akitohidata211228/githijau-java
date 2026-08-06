13d6e82bfb09ea70a4f2411a4e22026afc9c1e8f
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 18:53:24 +0700

    feat: add Tabel Perkalian 20 (Java)

---
+++ programs/MultiplicationTableOf20.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf20.java
+// Tabel perkalian 20.
+
+public class MultiplicationTableOf20 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("20 x " + i + " = " + (20 * i));
+        }
+    }
+}
