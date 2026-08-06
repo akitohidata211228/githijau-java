4bb568329ba50b8b4691d97b966943d979fb9a69
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 12:50:20 +0700

    feat: add Tabel Perkalian 16 (Java)

---
+++ programs/MultiplicationTableOf16.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf16.java
+// Tabel perkalian 16.
+
+public class MultiplicationTableOf16 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("16 x " + i + " = " + (16 * i));
+        }
+    }
+}
