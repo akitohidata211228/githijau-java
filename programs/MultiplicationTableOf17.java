f3fb8b25a8054ae213f16bd1164ac29a633ab2da
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 14:32:02 +0700

    feat: add Tabel Perkalian 17 (Java)

---
+++ programs/MultiplicationTableOf17.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf17.java
+// Tabel perkalian 17.
+
+public class MultiplicationTableOf17 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("17 x " + i + " = " + (17 * i));
+        }
+    }
+}
