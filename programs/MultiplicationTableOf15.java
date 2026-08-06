56117fa2182482ee4f7f5d7486a0cbbc0ed48474
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 12:38:20 +0700

    feat: add Tabel Perkalian 15 (Java)

---
+++ programs/MultiplicationTableOf15.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf15.java
+// Tabel perkalian 15.
+
+public class MultiplicationTableOf15 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("15 x " + i + " = " + (15 * i));
+        }
+    }
+}
