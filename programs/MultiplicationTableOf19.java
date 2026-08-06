ffa65859bcf1dd64a2c1099664d690f17830043e
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 17:54:24 +0700

    feat: add Tabel Perkalian 19 (Java)

---
+++ programs/MultiplicationTableOf19.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf19.java
+// Tabel perkalian 19.
+
+public class MultiplicationTableOf19 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("19 x " + i + " = " + (19 * i));
+        }
+    }
+}
