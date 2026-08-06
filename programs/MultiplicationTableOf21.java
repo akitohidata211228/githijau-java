273912732500a4a044a6ca44c9121ae865225818
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 21:08:24 +0700

    feat: add Tabel Perkalian 21 (Java)

---
+++ programs/MultiplicationTableOf21.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf21.java
+// Tabel perkalian 21.
+
+public class MultiplicationTableOf21 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("21 x " + i + " = " + (21 * i));
+        }
+    }
+}
