d10bff2b9152e94384025f6680caa7ce466fb488
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 12:26:20 +0700

    feat: add Tabel Perkalian 14 (Java)

---
+++ programs/MultiplicationTableOf14.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf14.java
+// Tabel perkalian 14.
+
+public class MultiplicationTableOf14 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("14 x " + i + " = " + (14 * i));
+        }
+    }
+}
