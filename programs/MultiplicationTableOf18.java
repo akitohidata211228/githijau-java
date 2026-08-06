2aeb43e4facd7dfe3d44864e707d5be1db392c07
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-06 16:25:11 +0700

    feat: add Tabel Perkalian 18 (Java)

---
+++ programs/MultiplicationTableOf18.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf18.java
+// Tabel perkalian 18.
+
+public class MultiplicationTableOf18 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("18 x " + i + " = " + (18 * i));
+        }
+    }
+}
