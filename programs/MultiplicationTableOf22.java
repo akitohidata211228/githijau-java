40b16f1c85532a4fc5738a0c1eaeda23b246c604
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 08:04:33 +0700

    feat: add Tabel Perkalian 22 (Java)

---
+++ programs/MultiplicationTableOf22.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf22.java
+// Tabel perkalian 22.
+
+public class MultiplicationTableOf22 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("22 x " + i + " = " + (22 * i));
+        }
+    }
+}
