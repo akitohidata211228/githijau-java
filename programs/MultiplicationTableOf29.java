efe112124816a4784848a30ad7d02025fd2dac59
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 20:09:12 +0700

    feat: add Tabel Perkalian 29 (Java)

---
+++ programs/MultiplicationTableOf29.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf29.java
+// Tabel perkalian 29.
+
+public class MultiplicationTableOf29 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("29 x " + i + " = " + (29 * i));
+        }
+    }
+}
