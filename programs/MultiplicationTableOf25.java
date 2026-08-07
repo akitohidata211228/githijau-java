d80e98f70d651dd38f27511dc72b85136514f776
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 13:38:42 +0700

    feat: add Tabel Perkalian 25 (Java)

---
+++ programs/MultiplicationTableOf25.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf25.java
+// Tabel perkalian 25.
+
+public class MultiplicationTableOf25 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("25 x " + i + " = " + (25 * i));
+        }
+    }
+}
