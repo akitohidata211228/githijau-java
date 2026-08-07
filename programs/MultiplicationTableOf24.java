a2de8f037e61307742450e3ac2b3bc4e53537393
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 11:43:42 +0700

    feat: add Tabel Perkalian 24 (Java)

---
+++ programs/MultiplicationTableOf24.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf24.java
+// Tabel perkalian 24.
+
+public class MultiplicationTableOf24 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("24 x " + i + " = " + (24 * i));
+        }
+    }
+}
