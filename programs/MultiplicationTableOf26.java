b5ba2bf24df936d0fc353c586d5f1b5574943da5
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 15:20:42 +0700

    feat: add Tabel Perkalian 26 (Java)

---
+++ programs/MultiplicationTableOf26.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf26.java
+// Tabel perkalian 26.
+
+public class MultiplicationTableOf26 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("26 x " + i + " = " + (26 * i));
+        }
+    }
+}
