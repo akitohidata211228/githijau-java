2851f454c0a595878caec74b75d9227fd7c0ff9c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-07 16:34:42 +0700

    feat: add Tabel Perkalian 27 (Java)

---
+++ programs/MultiplicationTableOf27.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf27.java
+// Tabel perkalian 27.
+
+public class MultiplicationTableOf27 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("27 x " + i + " = " + (27 * i));
+        }
+    }
+}
