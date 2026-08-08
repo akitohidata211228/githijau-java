3517d6f560eb358bf603ab1e892440ae3c76fa99
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:49:22 +0700

    feat: add Tabel Perkalian 36 (Java)

---
+++ programs/MultiplicationTableOf36.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf36.java
+// Tabel perkalian 36.
+
+public class MultiplicationTableOf36 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("36 x " + i + " = " + (36 * i));
+        }
+    }
+}
