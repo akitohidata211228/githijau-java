4239953d78457ac81c78e737a152a9065e0bed08
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 13:40:26 +0700

    feat: add Tabel Perkalian 32 (Java)

---
+++ programs/MultiplicationTableOf32.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf32.java
+// Tabel perkalian 32.
+
+public class MultiplicationTableOf32 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("32 x " + i + " = " + (32 * i));
+        }
+    }
+}
