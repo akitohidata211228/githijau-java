50fbd356a306ee8f80a07bf1ccf7d83ca5a5080f
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 20:25:21 +0700

    feat: add Tabel Perkalian 34 (Java)

---
+++ programs/MultiplicationTableOf34.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf34.java
+// Tabel perkalian 34.
+
+public class MultiplicationTableOf34 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("34 x " + i + " = " + (34 * i));
+        }
+    }
+}
