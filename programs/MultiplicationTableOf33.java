a721f5a13de01b542ac2138af61000f053c11682
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 13:52:26 +0700

    feat: add Tabel Perkalian 33 (Java)

---
+++ programs/MultiplicationTableOf33.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf33.java
+// Tabel perkalian 33.
+
+public class MultiplicationTableOf33 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("33 x " + i + " = " + (33 * i));
+        }
+    }
+}
