1f9de5ed8e9afbd620cb7e15e41c939f2460271b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-08 13:28:26 +0700

    feat: add Tabel Perkalian 31 (Java)

---
+++ programs/MultiplicationTableOf31.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf31.java
+// Tabel perkalian 31.
+
+public class MultiplicationTableOf31 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("31 x " + i + " = " + (31 * i));
+        }
+    }
+}
