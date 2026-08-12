d9119e21fd34ffff1b589de40d4d6912bcf485c2
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:57:03 +0700

    feat: add Tabel Perkalian 40 (Java)

---
+++ programs/MultiplicationTableOf40.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf40.java
+// Tabel perkalian 40.
+
+public class MultiplicationTableOf40 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("40 x " + i + " = " + (40 * i));
+        }
+    }
+}
