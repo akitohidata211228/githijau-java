dd45dae5c3df4d63ce712804ef5a4b0e278fa692
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:44:03 +0700

    feat: add Tabel Perkalian 39 (Java)

---
+++ programs/MultiplicationTableOf39.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf39.java
+// Tabel perkalian 39.
+
+public class MultiplicationTableOf39 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("39 x " + i + " = " + (39 * i));
+        }
+    }
+}
