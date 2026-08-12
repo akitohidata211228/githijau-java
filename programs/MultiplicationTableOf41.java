7a1417f6f45475a84080e6060db7a1c99a04b2ab
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 14:10:03 +0700

    feat: add Tabel Perkalian 41 (Java)

---
+++ programs/MultiplicationTableOf41.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf41.java
+// Tabel perkalian 41.
+
+public class MultiplicationTableOf41 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("41 x " + i + " = " + (41 * i));
+        }
+    }
+}
