91d05858494ea29a609ac0eecba8d06e1096100c
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 19:45:07 +0700

    feat: add Tabel Perkalian 44 (Java)

---
+++ programs/MultiplicationTableOf44.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf44.java
+// Tabel perkalian 44.
+
+public class MultiplicationTableOf44 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("44 x " + i + " = " + (44 * i));
+        }
+    }
+}
