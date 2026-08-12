619b83731077af22f2165a30545968bed8b5a2cf
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-12 13:31:03 +0700

    feat: add Tabel Perkalian 38 (Java)

---
+++ programs/MultiplicationTableOf38.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf38.java
+// Tabel perkalian 38.
+
+public class MultiplicationTableOf38 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("38 x " + i + " = " + (38 * i));
+        }
+    }
+}
