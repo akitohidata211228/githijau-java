43840399dba79f1fe4aca33da32dac11d9ff3829
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-13 14:49:25 +0700

    feat: add Tabel Perkalian 48 (Java)

---
+++ programs/MultiplicationTableOf48.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf48.java
+// Tabel perkalian 48.
+
+public class MultiplicationTableOf48 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("48 x " + i + " = " + (48 * i));
+        }
+    }
+}
