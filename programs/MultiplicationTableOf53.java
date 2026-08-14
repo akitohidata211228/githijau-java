bfda5af4caa75f09fcd4614ebaaa73635bf812b4
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 09:35:56 +0700

    feat: add Tabel Perkalian 53 (Java)

---
+++ programs/MultiplicationTableOf53.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf53.java
+// Tabel perkalian 53.
+
+public class MultiplicationTableOf53 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("53 x " + i + " = " + (53 * i));
+        }
+    }
+}
