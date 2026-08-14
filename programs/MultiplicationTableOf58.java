87f86e60e94ffb9b36f72247c71a551f54026f2d
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 19:03:09 +0700

    feat: add Tabel Perkalian 58 (Java)

---
+++ programs/MultiplicationTableOf58.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf58.java
+// Tabel perkalian 58.
+
+public class MultiplicationTableOf58 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("58 x " + i + " = " + (58 * i));
+        }
+    }
+}
