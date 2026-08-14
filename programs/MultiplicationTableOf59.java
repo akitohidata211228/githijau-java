eaa2d05140e405f2eeaacca3d93827260e172e01
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 21:03:10 +0700

    feat: add Tabel Perkalian 59 (Java)

---
+++ programs/MultiplicationTableOf59.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf59.java
+// Tabel perkalian 59.
+
+public class MultiplicationTableOf59 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("59 x " + i + " = " + (59 * i));
+        }
+    }
+}
