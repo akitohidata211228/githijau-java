f9da787a7eee6333a6f3cb45681648c001517310
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-14 14:09:09 +0700

    feat: add Tabel Perkalian 56 (Java)

---
+++ programs/MultiplicationTableOf56.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf56.java
+// Tabel perkalian 56.
+
+public class MultiplicationTableOf56 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("56 x " + i + " = " + (56 * i));
+        }
+    }
+}
