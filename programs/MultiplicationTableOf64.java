4821ac28832bfde94a21c620323450a59ffe76d0
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 16:32:11 +0700

    feat: add Tabel Perkalian 64 (Java)

---
+++ programs/MultiplicationTableOf64.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf64.java
+// Tabel perkalian 64.
+
+public class MultiplicationTableOf64 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("64 x " + i + " = " + (64 * i));
+        }
+    }
+}
