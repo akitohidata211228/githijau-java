74b002d2da169e0817d54db6105ad985c86855f2
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 08:19:35 +0700

    feat: add Tabel Perkalian 60 (Java)

---
+++ programs/MultiplicationTableOf60.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf60.java
+// Tabel perkalian 60.
+
+public class MultiplicationTableOf60 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("60 x " + i + " = " + (60 * i));
+        }
+    }
+}
