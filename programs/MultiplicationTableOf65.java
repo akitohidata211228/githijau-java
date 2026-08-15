cc653414695122dc8e7bfb85b1ca1f30bfb6eaa3
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 18:43:56 +0700

    feat: add Tabel Perkalian 65 (Java)

---
+++ programs/MultiplicationTableOf65.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf65.java
+// Tabel perkalian 65.
+
+public class MultiplicationTableOf65 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("65 x " + i + " = " + (65 * i));
+        }
+    }
+}
