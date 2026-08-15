f9f92dde8f1cdd69907b357fa00a418cffe3f69b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-15 12:22:24 +0700

    feat: add Tabel Perkalian 62 (Java)

---
+++ programs/MultiplicationTableOf62.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf62.java
+// Tabel perkalian 62.
+
+public class MultiplicationTableOf62 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("62 x " + i + " = " + (62 * i));
+        }
+    }
+}
