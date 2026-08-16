3c08145a83b859b34c953e0ffe2444ee82ee1d8b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-16 16:54:30 +0700

    feat: add Tabel Perkalian 72 (Java)

---
+++ programs/MultiplicationTableOf72.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf72.java
+// Tabel perkalian 72.
+
+public class MultiplicationTableOf72 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("72 x " + i + " = " + (72 * i));
+        }
+    }
+}
