131fc59749ab938af4100e48f2ec9a3d11ba1974
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-16 20:09:43 +0700

    feat: add Tabel Perkalian 74 (Java)

---
+++ programs/MultiplicationTableOf74.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf74.java
+// Tabel perkalian 74.
+
+public class MultiplicationTableOf74 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("74 x " + i + " = " + (74 * i));
+        }
+    }
+}
