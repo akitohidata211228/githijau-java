63d40bed8bd1d1d4dac86b7351e5cd61df3a11a1
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-16 12:35:30 +0700

    feat: add Tabel Perkalian 70 (Java)

---
+++ programs/MultiplicationTableOf70.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf70.java
+// Tabel perkalian 70.
+
+public class MultiplicationTableOf70 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("70 x " + i + " = " + (70 * i));
+        }
+    }
+}
