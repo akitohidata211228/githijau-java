94582e235f7655c36772e20fb876c5296e1e5355
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 16:58:44 +0700

    feat: add Tabel Perkalian 79 (Java)

---
+++ programs/MultiplicationTableOf79.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf79.java
+// Tabel perkalian 79.
+
+public class MultiplicationTableOf79 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("79 x " + i + " = " + (79 * i));
+        }
+    }
+}
