9bbd840c44c285d5f9f11f803d4c5f64565f0f95
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-17 19:35:47 +0700

    feat: add Tabel Perkalian 81 (Java)

---
+++ programs/MultiplicationTableOf81.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf81.java
+// Tabel perkalian 81.
+
+public class MultiplicationTableOf81 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("81 x " + i + " = " + (81 * i));
+        }
+    }
+}
