37f3d4237a2d9a2d64b8535245536275e00901bb
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 16:14:29 +0700

    feat: add Tabel Perkalian 87 (Java)

---
+++ programs/MultiplicationTableOf87.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf87.java
+// Tabel perkalian 87.
+
+public class MultiplicationTableOf87 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("87 x " + i + " = " + (87 * i));
+        }
+    }
+}
