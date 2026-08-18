72d8c41cff7f58a0e2a766592675c25f291a62cd
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 08:03:28 +0700

    feat: add Tabel Perkalian 83 (Java)

---
+++ programs/MultiplicationTableOf83.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf83.java
+// Tabel perkalian 83.
+
+public class MultiplicationTableOf83 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("83 x " + i + " = " + (83 * i));
+        }
+    }
+}
