76c4e4317b3c9ca308a95250e6f2c05c8534ce90
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 19:29:33 +0700

    feat: add Tabel Perkalian 89 (Java)

---
+++ programs/MultiplicationTableOf89.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf89.java
+// Tabel perkalian 89.
+
+public class MultiplicationTableOf89 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("89 x " + i + " = " + (89 * i));
+        }
+    }
+}
