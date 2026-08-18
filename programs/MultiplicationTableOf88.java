52394a1dcd3fcbfd20180d2ce20a4d61277688d1
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 17:26:29 +0700

    feat: add Tabel Perkalian 88 (Java)

---
+++ programs/MultiplicationTableOf88.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf88.java
+// Tabel perkalian 88.
+
+public class MultiplicationTableOf88 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("88 x " + i + " = " + (88 * i));
+        }
+    }
+}
