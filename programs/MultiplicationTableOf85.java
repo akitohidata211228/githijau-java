cf102415dc2991bd3f35b03b18e45bf7e31b4b4b
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-18 12:26:28 +0700

    feat: add Tabel Perkalian 85 (Java)

---
+++ programs/MultiplicationTableOf85.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf85.java
+// Tabel perkalian 85.
+
+public class MultiplicationTableOf85 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("85 x " + i + " = " + (85 * i));
+        }
+    }
+}
