312da51eb005bba5a4baa22bcfdacce49d067ca5
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 19:17:42 +0700

    feat: add Tabel Perkalian 97 (Java)

---
+++ programs/MultiplicationTableOf97.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf97.java
+// Tabel perkalian 97.
+
+public class MultiplicationTableOf97 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("97 x " + i + " = " + (97 * i));
+        }
+    }
+}
