506a286a8f31aa3e62b9ba8f0ba327fc9ea5196a
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 08:59:01 +0700

    feat: add Tabel Perkalian 91 (Java)

---
+++ programs/MultiplicationTableOf91.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf91.java
+// Tabel perkalian 91.
+
+public class MultiplicationTableOf91 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("91 x " + i + " = " + (91 * i));
+        }
+    }
+}
