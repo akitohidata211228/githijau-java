bfbc4c1b954f4c878557f6a31a241636e4bf8ee9
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 22:05:42 +0700

    feat: add Tabel Perkalian 98 (Java)

---
+++ programs/MultiplicationTableOf98.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf98.java
+// Tabel perkalian 98.
+
+public class MultiplicationTableOf98 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("98 x " + i + " = " + (98 * i));
+        }
+    }
+}
