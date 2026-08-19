cae8153e43da7d3334f3adaab5a637d71e6b4ec2
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-19 18:26:53 +0700

    feat: add Tabel Perkalian 96 (Java)

---
+++ programs/MultiplicationTableOf96.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf96.java
+// Tabel perkalian 96.
+
+public class MultiplicationTableOf96 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("96 x " + i + " = " + (96 * i));
+        }
+    }
+}
