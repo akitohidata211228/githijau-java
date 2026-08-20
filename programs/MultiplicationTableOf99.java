adaa81b9d32fbaaca4671a55973c00ce8db47c94
Muhammad Taufik Hidayat <yarey614@gmail.com>
2026-08-20 09:24:44 +0700

    feat: add Tabel Perkalian 99 (Java)

---
+++ programs/MultiplicationTableOf99.java
@@ -0,0 +1,10 @@
+// MultiplicationTableOf99.java
+// Tabel perkalian 99.
+
+public class MultiplicationTableOf99 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("99 x " + i + " = " + (99 * i));
+        }
+    }
+}
